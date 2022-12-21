7f851924dca21ced5b7f013badf42c2a24a70a4c
mayados <115099504+mayados@users.noreply.github.com>
2022-12-21 18:58:16 +0100

    add js code

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function afficherMenu() {
+    const menuNavigation = document.querySelector('#menu-navigation');
+    const burger = document.querySelector('.burger');
+  
+    burger.addEventListener('click', (event) => {    
+      menuNavigation.classList.toggle('show-nav');
+    });    
+  } 
+  
+  afficherMenu();
